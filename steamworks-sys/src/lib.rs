a2cee91cfb744df0871085ee1482d79af51023ca
Brian Bowman <seeker14491@gmail.com>
2019-09-02 22:59:18 -0500

    Silence some warnings in bindgen-generated code

--- steamworks-sys/src/lib.rs
+++ steamworks-sys/src/lib.rs
@@ -1,4 +1,6 @@
 #![allow(non_camel_case_types, non_snake_case, non_upper_case_globals)]
 #![allow(clippy::all)]
 
+#![allow(deprecated, invalid_value)] // Silence warning generated as of bindgen 0.51
+
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
