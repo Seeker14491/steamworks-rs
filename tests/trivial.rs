82aaf7fdfebf6ff2e263eb6e35166f097a280c2a
Brian Bowman <seeker14491@gmail.com>
2020-09-06 00:52:19 -0500

    Add a trivial integration test

---
+++ tests/trivial.rs
@@ -0,0 +1,9 @@
+use std::io::Write;
+
+#[test]
+fn initialization_succeeds_or_fails_gracefully() {
+    steamworks::Client::init().ok();
+
+    // tidy test output, as the Steam API writes to the console
+    std::io::stderr().write_all(b"\n\n").ok();
+}
